2594b82e259b5f19ab9dec9e12a56feb5d22fae4
shonmikl <mikhailegorov676@gmail.com>
2023-06-22 20:25:57 +0200

    taski

--- src/main/java/org/example/Test1.java
+++ src/main/java/org/example/Test1.java
@@ -1,32 +1,20 @@
 package org.example;
 
-import org.example._2023_06_08.POJO;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Test1 {
-    private void getArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 15) {
-                System.out.println("!!!");
-            }
-        }
+    void m1(){
+    }
+
+    static void m2() {
     }
 
     public static void main(String[] args) {
-
-        int[] array = {2, 4, 1, 2, 7};
-
-
-        for (int i = 0; i < 10; i++) {
-
-        }
-//             TYPE x = new 5(constructor)
-        POJO pojo = new POJO("II", 11);
-        pojo.setSalary(9999999);
-        new POJO("WS", 654d).setSalary(123);
-
+        m2();
+        Test55555.m3();
+        new Test1().m1();
     }
+}
 
+class Test55555 {
+    static void m3() {
+    }
 }
