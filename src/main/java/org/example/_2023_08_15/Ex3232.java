11159bd4ce28fe78113c76cefcae138df8541ffb
shonmikl <mikhailegorov676@gmail.com>
2023-08-15 19:58:22 +0200

    test2

---
+++ src/main/java/org/example/_2023_08_15/Ex3232.java
@@ -0,0 +1,19 @@
+package org.example._2023_08_15;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+public class Ex3232 {
+    public static void main(String[] args) {
+
+    }
+}
+@AllArgsConstructor
+class Order {
+    List<Product> productList;
+}
+
+class Product {
+    private double price;
+}
